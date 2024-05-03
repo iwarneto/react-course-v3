ac69977fce8febf6ea15747da892bfb65ca565c5
unknown <iwarneto@gmail.com>
2024-05-03 17:39:49 -0300

    realizar curso

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,7 +1,10 @@
+import Starter from './tutorial/03-conditional-rendering/starter/07-user-challenge'
+import Final from './tutorial/03-conditional-rendering/final/07-user-challenge'
+
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced React</h2>
+      <Starter />
     </div>
   );
 }
--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,24 @@
+import { useState, useEffect } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+
+  const user = "Vegan Food Truck"
+
+  function logInOut() {
+    setIsLoggedIn(state => !state)
+  }
+
+  return (
+    <div>
+      {!isLoggedIn ? <h4>Please Login</h4> : <h4>Hello there, {user}!</h4>}
+      {
+      !isLoggedIn ?
+      <button onClick={logInOut} className="btn">Login</button> :
+      <button onClick={logInOut} className="btn">Logout</button>
+      }
+    </div>
+  );
 };
 
 export default UserChallenge;
